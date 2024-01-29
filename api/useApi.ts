b7a4161be1f272f6d0340c14068a106089c96149
Walter Celi <antonini2903@hotmail.com>
2024-01-29 12:01:18 -0500

    Agregados módulos comunes de API
    - Rotas de API
    - Llamadas a API
    - interfaces y tipos

---
+++ api/useApi.ts
@@ -0,0 +1,84 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+import {
+  ApiErrorResponse,
+  AuthInterface,
+  TokenResponse,
+} from "../interfaces/models";
+import { useAuth } from "../context/AuthContext";
+import { Endpoints } from "./routes";
+
+import 'rsuite/dist/rsuite.min.css';
+
+
+export const useApiController = (authContext) => {
+  //#region AxiosConfig
+  const { login,  UserData } = authContext;
+  const ApiTokenRequest = axios.create({
+    baseURL: Endpoints.BaseURL + Endpoints.Api + Endpoints.login,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: "POST",
+  });
+
+  const ApiRequest = axios.create({
+    baseURL: Endpoints.BaseURL + Endpoints.Api,
+    headers: {
+      "Content-Type": "application/json",
+      ...(UserData?.access_token !== ""  && UserData?.access_token !==undefined? { Authorization: `Bearer ${UserData?.access_token}` } : {}),
+    },
+  });
+
+  const ApiPostFileRequest = axios.create({
+    baseURL: Endpoints.BaseURL + Endpoints.Api,
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${UserData?.access_token}`,
+    },
+  });
+
+  const getData = <T>(request: Promise<I>): Promise<T> => request
+    .then(({ data }: AxiosResponse<T>) => data)
+    .catch((e) => { throw e })
+  const get = <T>(endpoint: string, params?: object,): Promise<T> => 
+    getData(ApiRequest.get(endpoint, { params }));
+  const post = <T>(endpoint: string, data?: object, params?: object): Promise<T> => 
+    getData(ApiRequest.post(endpoint, data, { params }));
+  const patch = <T>(endpoint: string, data?: object, params?: object): Promise<T> => 
+    getData(ApiRequest.patch(endpoint, data, { params }));
+  const remove = <T>(endpoint: string, params?: object): Promise<T> => 
+    getData(ApiRequest.delete(endpoint, { params }));
+
+  const postRequestToken = async <T extends TokenResponse>(
+    data: AuthInterface
+  ): Promise<T> => {
+    return await ApiTokenRequest.request({
+      data,
+    })
+      .then(({ data }: AxiosResponse<T>) => {
+        login(data);
+        console.log(data);
+        return data;
+      })
+      .catch((error: AxiosError<ApiErrorResponse>) => {
+        console.log(JSON.stringify(error, null, 3));
+        throw error;
+      })
+  };
+
+  const postFileRequest = async <T>(
+    endpoint: string,
+    data?: object,
+    params?: object
+  ): Promise<T> => {
+    return await ApiPostFileRequest.post(endpoint, data, { params })
+      .then(({ data }: AxiosResponse<T>) => data)
+      .catch((error: AxiosError<ApiErrorResponse>) => {
+        throw error;
+      })
+  };
+
+  return { get, post, patch, remove, postRequestToken, postFileRequest };
+};
---
+++ helpers/utils.ts
@@ -0,0 +1,11 @@
+
+export const printStackTrace = (e: Error): string[] => 
+  e.stack?.split("\n").slice(1).map(s => s.replace(/^\s+at\s/, "")) || []
+
+interface indexedT {
+  id: number;
+}
+export const arrayIndexer = <T extends indexedT>(values: T[]): {[key: number]: T[]} => 
+  values.reduce( (actual, cur, i) => Object.assign(actual, { [cur.id]: i} ), {} )
+
+
