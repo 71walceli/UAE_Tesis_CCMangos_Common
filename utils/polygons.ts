14a6406ffbfb345c1e6983264a3556365195fd62
Walter Celi <antonini2903@hotmail.com>
2024-07-28 18:24:17 -0500

    Funciones de áreas, perlimetros y centros

---
+++ utils/polygons.ts
@@ -0,0 +1,31 @@
+import {center, area, length} from "@turf/turf";
+
+export const parsePolygon = (string: string) => string.trim() !== ""
+  ?string.split(";").map(coord => coord.split(":"))
+    .map(([lat, lng]) => ({ lat: Number(lat), lng: Number(lng) }))
+  :[]
+
+export const getCenter = (polygon: {lat: number, lng: number}[]) => center({ 
+  type: "Polygon", 
+  coordinates: [polygon.map(({ lat, lng }) => [lng, lat])] 
+}).geometry.coordinates.reverse()
+
+/**
+ * @param polygon {lat: number, lng: number}[]
+ * @returns length in kilometers
+ */
+export const getLength = (polygon: {lat: number, lng: number}[]) => length({ 
+  type: "Polygon", 
+  coordinates: [polygon.map(({ lat, lng }) => [lng, lat])],
+  //units: "kilometers",
+})
+
+/**
+ * @param polygon {lat: number, lng: number}[]
+ * @returns area in square meters
+ */
+export const getArea = (polygon: {lat: number, lng: number}[]) => area({ 
+  type: "Polygon", 
+  coordinates: [polygon.map(({ lat, lng }) => [lng, lat])],
+  //units: "hectares",
+})
